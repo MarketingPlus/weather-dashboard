ffa8072db95b0a77917b7696d42e750cd6716245
MarketingPlus <benmarketingplus@gmail.com>
2021-04-04 20:25:59 +1000

    added my API Key and links to make sure they work properly I have also included a little icon to display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,23 @@
 var today = moment().format('L');
 var searchHistoryList = [];
-var apiKey = "";
+var apiKey = "0359154709f46ac528722b59c9dd068d";
 
 // function to check for the current weather condition
 function currentCondition(city) {
-    var queryURL = '';
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
     $.ajax({
         url: gueryURL,
         method: "GET"
-    }).then(function(cityWeatherResponse)) {
-        console.log(cityWeatherResponse);
-    }
+    }).then(function(cityWeatherResponse) {
+        console.log(cityWeatherResponse); 
+
+        $("#weatherContent").css("display", "block");
+        $("#cityDetail").empty();
+
+        var iconCode = cityWeatherResponse.weather[0].icon;
+        var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+
+    })
 }
