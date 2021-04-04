d709801cbaa88b354939faf58f4c103111a7a4f5
MarketingPlus <benmarketingplus@gmail.com>
2021-04-04 20:34:34 +1000

    added all the infomation for windspeed, temp and humidity for the api to pull from and display which appends after eachother

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,30 @@ function currentCondition(city) {
         var iconCode = cityWeatherResponse.weather[0].icon;
         var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
 
+        var currentCity = $(`
+            <h2 id="currentCity">
+                ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+            </h2>
+            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+            <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
+            <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+        `);
+
+        $("#cityDetail").append(currentCity);
+
+        // UV Index
+        var lat = cityWeatherResponse.lat
+        var lon = cityWeatherResponse.long
+        var uviQueryURL = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+        $.ajax({
+            url: uviQueryURL,
+            method: "GET"
+        }).then(function(uviResponse) {
+            console.log(uviResponse);
+
+            var uvIndex = uviResponse.value;
+        })
 
     })
 }
