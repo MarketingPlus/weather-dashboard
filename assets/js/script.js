927d739450794dd326cb8417fa702ee97912c9f1
MarketingPlus <benmarketingplus@gmail.com>
2021-04-04 20:20:24 +1000

    started adding my function however i need to generate an api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,15 @@
 var today = moment().format('L');
 var searchHistoryList = [];
+var apiKey = "";
+
+// function to check for the current weather condition
+function currentCondition(city) {
+    var queryURL = '';
+
+    $.ajax({
+        url: gueryURL,
+        method: "GET"
+    }).then(function(cityWeatherResponse)) {
+        console.log(cityWeatherResponse);
+    }
+}
