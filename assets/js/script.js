c1712187b2c8c4445aac4426b84f361270e08fd9
MarketingPlus <benmarketingplus@gmail.com>
2021-04-04 21:04:35 +1000

    added the last seen so the user can have their searches saved on the side bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,7 @@ function currentCondition(city) {
             <h2 id="currentCity">
                 ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
             </h2>
-            <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+            <p>Temperature: ${cityWeatherResponse.main.temp} °C</p>
             <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
             <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
         `);
@@ -91,14 +91,14 @@ function futureCondition(lat, lon) {
 
             var currDate = moment.unix(cityInfo.date).format("DD/MM/YYYY");
             var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
-            var calulation = (${cityInfo.temp}-32)*5/9
+            
             var futureCard = $(`
                 <div class="pl-3">
                     <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
                         <div class="card-body">
                             <h5>${currDate}</h5>
                             <p>${iconURL}</p>
-                            <p>Temp: ${calulation} °C</p>
+                            <p>Temp:${cityInfo.temp}°C</p>
                             <p>Humidity: ${cityInfo.humidity}\%</p>
                         </div>
                     </div>
@@ -133,4 +133,16 @@ $("#searchBtn").on("click", function(event) {
 $(document).on("click", ".list-group-item", function() {
     var listCity = $(this).text();
     currentCondition(listCity);
+});
+
+// last search on the dashboard
+$(document).ready(function() {
+    var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+
+    if (searchHistoryArr !== null) {
+        var lastSearchedIndex = searchHistoryArr.length - 1;
+        var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+        currentCondition(lastSearchedCity);
+        console.log(`Last searched city: ${lastSearchedCity}`);
+    }
 });
